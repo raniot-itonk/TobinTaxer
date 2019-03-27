55df6eead73841be8c6686dd7d9ea711a11a3326
Anders Meidahl <Andersmeidahl@gmail.com>
2019-03-27 19:17:02 +0100

    changed database setup

--- TobinTaxer/Controllers/StockTaxesController.cs
+++ TobinTaxer/Controllers/StockTaxesController.cs
@@ -34,50 +34,59 @@ namespace TobinTaxer.Controllers
         [HttpPost]
         public async Task<ActionResult<TaxHistory>> PostStockTax(StockTaxObject stockTaxObject)
         {
-            // Call Bank service
-            var tax = stockTaxObject.Price * stockTaxObject.Amount * _taxInfo.TaxRate;
-
-            var url = _services.BankService.BaseAddress + "api/transfer";
-            var stateAccount = Guid.Parse("7bedb953-4e7e-45f9-91de-ffc0175be744");
-            var transferObject = new TransferObject { Amount = tax, FromAccountId = stockTaxObject.Buyer, ReservationId = stockTaxObject.ReservationId, ToAccountId = stateAccount };
             try
             {
-                var response = await Policy.Handle<FlurlHttpException>()
-                    .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
-                    .WaitAndRetryAsync(new[]
-                    {
+                // Call Bank service
+                var tax = stockTaxObject.Price * stockTaxObject.Amount * _taxInfo.TaxRate;
+
+                var url = _services.BankService.BaseAddress + "api/transfer";
+                var stateAccount = Guid.Parse("7bedb953-4e7e-45f9-91de-ffc0175be744");
+                var transferObject = new TransferObject { Amount = tax, FromAccountId = stockTaxObject.Buyer, ReservationId = stockTaxObject.ReservationId, ToAccountId = stateAccount };
+                try
+                {
+                    var response = await Policy.Handle<FlurlHttpException>()
+                        .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                        .WaitAndRetryAsync(new[]
+                        {
                         TimeSpan.FromSeconds(1),
                         TimeSpan.FromSeconds(2),
                         TimeSpan.FromSeconds(3)
-                    }).ExecuteAsync(() => url.WithTimeout(10).PutJsonAsync(transferObject));
+                        }).ExecuteAsync(() => url.WithTimeout(10).PutJsonAsync(transferObject));
 
-                if (!response.IsSuccessStatusCode) return BadRequest("Failed to Tax trade");
+                    if (!response.IsSuccessStatusCode) return BadRequest("Failed to Tax trade");
+                }
+                catch (FlurlHttpException e)
+                {
+                    Console.WriteLine(e.Call.Response);
+                    Console.WriteLine(e);
+                    throw;
+                }
+
+
+                // Store in own database
+                var taxHistory = new TaxHistory
+                {
+                    Amount = stockTaxObject.Amount,
+                    Buyer = stockTaxObject.Buyer,
+                    Price = stockTaxObject.Price,
+                    Seller = stockTaxObject.Seller,
+                    StockId = stockTaxObject.StockId,
+                    TaxRate = _taxInfo.TaxRate,
+                    Tax = tax
+                };
+                await _context.TaxHistories.AddAsync(taxHistory);
+                await _context.SaveChangesAsync();
+
+                _logger.LogInformation("Exacted {Tax in taxes from from {User}", tax, stockTaxObject.Buyer);
+                _logger.LogInformation("Logged TaxInfo to database {TaxHistory}", taxHistory);
+                return taxHistory;
             }
-            catch (FlurlHttpException e)
+            catch (Exception e)
             {
-                Console.WriteLine(e.Call.Response);
                 Console.WriteLine(e);
                 throw;
             }
-            
 
-            // Store in own database
-            var taxHistory = new TaxHistory
-            {
-                Amount = stockTaxObject.Amount,
-                Buyer = stockTaxObject.Buyer,
-                Price = stockTaxObject.Price,
-                Seller = stockTaxObject.Seller,
-                StockId = stockTaxObject.StockId,
-                TaxRate = _taxInfo.TaxRate,
-                Tax = tax 
-            };
-            await _context.TaxHistories.AddAsync(taxHistory);
-            await _context.SaveChangesAsync();
-
-            _logger.LogInformation("Exacted {Tax in taxes from from {User}", tax, stockTaxObject.Buyer);
-            _logger.LogInformation("Logged TaxInfo to database {TaxHistory}", taxHistory);
-            return taxHistory;
         }
     }
 }
--- TobinTaxer/Startup.cs
+++ TobinTaxer/Startup.cs
@@ -57,20 +57,21 @@ namespace TobinTaxer
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, TobinTaxerContext context)
         {
             app.UseAuthentication();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
             }
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                context.Database.Migrate();
             }
-
-            InitializeDatabase(app, env);
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
@@ -84,17 +85,6 @@ namespace TobinTaxer
             app.UseMvc();
         }
 
-        private static void InitializeDatabase(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-            {
-                if(env.IsDevelopment())
-                    scope.ServiceProvider.GetRequiredService<TobinTaxerContext>().Database.EnsureCreated();
-                else
-                    scope.ServiceProvider.GetRequiredService<TobinTaxerContext>().Database.Migrate();
-
-            }
-        }
         private void SetupDatabase(IServiceCollection services, IHostingEnvironment env)
         {
             if(env.IsDevelopment())
